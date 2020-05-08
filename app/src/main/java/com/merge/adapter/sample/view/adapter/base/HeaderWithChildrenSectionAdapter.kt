2fd492d30e0492fd981e9b30f7e76f643356b07f
Hitesh <hiteshdas1912@gmail.com>
2020-05-08 19:42:38 +0530

    Removed unused imports

--- app/src/main/java/com/merge/adapter/sample/view/adapter/base/HeaderWithChildrenSectionAdapter.kt
+++ app/src/main/java/com/merge/adapter/sample/view/adapter/base/HeaderWithChildrenSectionAdapter.kt
@@ -1,15 +1,12 @@
 package com.merge.adapter.sample.view.adapter.base
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.MergeAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.merge.adapter.sample.R
 import com.merge.adapter.sample.view.adapter.base.BaseRecyclerViewAdapter.BaseVH
-import com.merge.adapter.sample.view.adapter.base.HeaderWithChildrenSectionAdapter.*
+import com.merge.adapter.sample.view.adapter.base.HeaderWithChildrenSectionAdapter.SectionVH
 import com.merge.adapter.sample.view.adapter.base.layoutmanager.CustomGridLayoutManager
-import com.merge.adapter.sample.view.base.CustomSpanSizeLookup
 
 abstract class HeaderWithChildrenSectionAdapter<HeaderAdapter : BaseRecyclerViewAdapter<out BaseVH>,
                                                 ChildrenAdapter : BaseRecyclerViewAdapter<out BaseVH>>
